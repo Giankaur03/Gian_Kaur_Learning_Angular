9e9aa7429fce87964af7f5db0098c93434b26c9f
Giankaur03 <W0852088@myscc.ca>
2024-09-27 18:00:51 -0400

    Used one way binding.

--- src/app/icecream-list/icecream-list.component.ts
+++ src/app/icecream-list/icecream-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {Icecream} from "../Shared/Modules/icecream";
+import {IcecreamListItemComponent} from "../icecream-list-item/icecream-list-item.component";
 
 @Component({
   selector: 'app-icecream-list',
   standalone: true,
-  imports: [],
+  imports: [
+    IcecreamListItemComponent
+  ],
   templateUrl: './icecream-list.component.html',
   styleUrl: './icecream-list.component.css'
 })
@@ -17,6 +20,7 @@ export class IcecreamListComponent {
     {id: 3, flavour: "Cookie and Cream", brand: "Baskin Robins", price: 4.75, toppings: false},
     {id: 4, flavour: "Strawberry", brand: "Blue Bell", price: 3.25, toppings:true}
   ];
+  selectedIcecream: any;
 
 
 }
