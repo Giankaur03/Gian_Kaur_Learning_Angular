20b5ea598e73bb6906be374864f956903b1399ea
Giankaur03 <W0852088@myscc.ca>
2024-10-09 19:30:42 -0400

    Added the array to the mock-icecream.ts and created the new file IcecreamService.ts.

---
+++ src/app/Shared/Modules/mock-icecream.ts
@@ -0,0 +1,8 @@
+import {Icecream} from "./icecream";
+
+ export const mockIcecream :Icecream[] = [
+  {id: 1, flavour: "Vanilla", brand: "Breyers", price: 3.00, toppings: false},
+  {id: 2, flavour: "Chocolate", brand: "Magnum", price: 4.50, toppings: true},
+  {id: 3, flavour: "Cookie and Cream", brand: "Baskin Robins", price: 4.75, toppings: false},
+  {id: 4, flavour: "Strawberry", brand: "Blue Bell", price: 3.25, toppings:true}
+]
--- src/app/icecream-list/icecream-list.component.ts
+++ src/app/icecream-list/icecream-list.component.ts
@@ -17,13 +17,7 @@ import {NgForOf, NgStyle} from "@angular/common";
 export class IcecreamListComponent {
 
   displayedColumns:string[]= ['id', 'flavour', 'brand', 'price', 'toppings'];
-  IcecreamList: Icecream[] = [
-    {id: 1, flavour: "Vanilla", brand: "Breyers", price: 3.00, toppings: false},
-    {id: 2, flavour: "Chocolate", brand: "Magnum", price: 4.50, toppings: true},
-    {id: 3, flavour: "Cookie and Cream", brand: "Baskin Robins", price: 4.75, toppings: false},
-    {id: 4, flavour: "Strawberry", brand: "Blue Bell", price: 3.25, toppings:true}
-  ];
+  IcecreamList: Icecream[] = [];
   selectedIcecream: any;
-
 
 }
