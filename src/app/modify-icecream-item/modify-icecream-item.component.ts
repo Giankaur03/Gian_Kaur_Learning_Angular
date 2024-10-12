c4fbec3b98d1b1d64213601c6e0914d35fa985de
Giankaur03 <W0852088@myscc.ca>
2024-10-11 22:35:04 -0400

    Added the link.

--- src/app/modify-icecream-item/modify-icecream-item.component.ts
+++ src/app/modify-icecream-item/modify-icecream-item.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-modify-icecream-item',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './modify-icecream-item.component.html',
   styleUrl: './modify-icecream-item.component.css'
 })
 export class ModifyIcecreamItemComponent {
+  icecreamForm: any;
 
+  onSubmit() {
+
+  }
+
+  onDelete() {
+
+  }
+
+  navigateToIcecreamList() {
+
+  }
 }
 
 export class ModifyIcecreamComponent {
