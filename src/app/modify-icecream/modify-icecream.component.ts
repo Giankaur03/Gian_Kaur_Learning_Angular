a24e0a0204d42b7ba74de78b128b535fb61fc074
Giankaur03 <W0852088@myscc.ca>
2024-11-18 16:41:29 -0500

    Created form

--- src/app/modify-icecream/modify-icecream.component.ts
+++ src/app/modify-icecream/modify-icecream.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {IcecreamList} from "../Shared/Modules/mock-icecream";
+import {ActivatedRoute, Router} from "@angular/router";
+import {IceCreamService} from "../Services/IcecreamService";
 
 @Component({
   selector: 'app-modify-icecream',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './modify-icecream.component.html',
   styleUrl: './modify-icecream.component.css'
 })
-export class ModifyIcecreamComponent {
+export class ModifyIcecreamComponent implements OnInit{
+  IcecreamList: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private icecreamService: IceCreamService,
+    private router: Router
+  ) {
+    this.IcecreamList = this.fb.group({
+      id:[''],
+      flavour: ['', Validators.required],
+      brand: ['', Validators.required],
+      price: ['', Validators.required],
+      toppings: [false]
+    });
+  }
+
+  onSubmit() {
+
+  }
+
+  navigateToIcecreamList() {
+    this.router.navigate(['/icecream'])
+
+  }
+
+  ngOnInit(): void {
+  }
 }
